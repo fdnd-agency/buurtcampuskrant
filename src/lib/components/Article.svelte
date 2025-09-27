e4708792965e83fdf8980fd8e60092e9fe53cecb
RickFDND <rick.pijnacker@hva.nl>
2025-09-27 14:31:19 +0200

    Component gecreëerd met basic HTML #14

---
+++ src/lib/components/Article.svelte
@@ -0,0 +1,11 @@
+<article>
+    <img src="https://images.squarespace-cdn.com/content/v1/66277968847b3143bb8b0b11/540ebc56-3661-434f-95cc-d99f14d1eed1/Buurtcampus+Raad+1.jpeg?format=2500w" alt="">
+    <h1>Title article</h1>
+    <p>Lorem ipsum dolor sit amet, consect badipiscing elit. Lorem ipsum consect badipiscing elit. </p>
+
+    <div>
+        <a href="/oost">oost</a>
+        <a href="/wijk">wijk</a>
+    </div>
+</article>
+
