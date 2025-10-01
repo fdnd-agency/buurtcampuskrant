a25c3ccebb5ca44070fa04f8fb47ced9147f72b7
Akikosophia <asschermer@hotmail.com>
2025-10-01 12:55:10 +0200

    #16 chore: add Filter component with articles prop

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+
+export let artikelen = [];
+import Filter from '$lib/components/Filter.svelte';
+    
+</script>
+
+<Filter {artikelen} />
+
