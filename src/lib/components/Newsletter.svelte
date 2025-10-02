57415e2bcbd10308d8ab0df050017c2a6aacd779
RickFDND <rick.pijnacker@hva.nl>
2025-10-02 20:22:19 +0200

    component aangemaakt en basic HTML #20

---
+++ src/lib/components/Newsletter.svelte
@@ -0,0 +1,14 @@
+<form action="/subscribe" method="post">
+    <!-- svelte-ignore a11y_consider_explicit_label -->
+    <button type="submit">
+        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M2.33333 3.66665L7.59533 7.69065C7.71156 7.77942 7.85375 7.82751 8 7.82751C8.14625 7.82751 8.28844 7.77942 8.40467 7.69065L13.6667 3.66665M2.66667 12.6666H13.3333C13.5101 12.6666 13.6797 12.5964 13.8047 12.4714C13.9298 12.3464 14 12.1768 14 12V3.99998C14 3.82317 13.9298 3.6536 13.8047 3.52858C13.6797 3.40355 13.5101 3.33331 13.3333 3.33331H2.66667C2.48986 3.33331 2.32029 3.40355 2.19526 3.52858C2.07024 3.6536 2 3.82317 2 3.99998V12C2 12.1768 2.07024 12.3464 2.19526 12.4714C2.32029 12.5964 2.48986 12.6666 2.66667 12.6666Z" stroke="#21284A" stroke-width="2" stroke-linecap="round"/>
+        </svg>
+    </button>
+
+    <input type="email" name="email" placeholder="e-mailadres" required>
+</form>
+
+<style>
+    
+</style>
