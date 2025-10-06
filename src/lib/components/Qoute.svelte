f235f7d341031f9a92a0565d2deb15455c4de941
Akikosophia <asschermer@hotmail.com>
2025-10-06 12:44:41 +0200

    #15 feat: add Quote component with semantic HTML structure

---
+++ src/lib/components/Qoute.svelte
@@ -0,0 +1,18 @@
+
+<div>
+    <blockquote>
+        <p></p>
+    </blockquote>
+</div>
+
+<style>
+
+div {
+    width: 300px;
+    height: 300px;
+    background-color: pink;
+    border-radius: 20px;
+}
+
+
+</style>
