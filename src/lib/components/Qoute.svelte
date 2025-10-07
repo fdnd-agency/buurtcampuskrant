138ec1433bb66f514a5a55ee75a30c55c2168a9d
Akikosophia <asschermer@hotmail.com>
2025-10-07 23:47:15 +0200

    #15 add clamp

--- src/lib/components/Qoute.svelte
+++ src/lib/components/Qoute.svelte
@@ -6,12 +6,16 @@
 
 <style>
 
+
+
 article {
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    /* margin: 0 auto; */
+    font-size: 16px;
+    font-family: "Arial", sans-serif;
+    font-style: italic;
     width: 300px;
     height: 100px;
     background-color: hsla(239, 33%, 30%, 1);
@@ -21,6 +25,7 @@ article {
 p {
     text-align: center;
     color: white;
+    max-width: clamp(40ch, 90%, 60ch);
 }
 
 
