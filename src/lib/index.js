69562033edc8df86145f5ecef7e02d5a7a110d5d
RickFDND <rick.pijnacker@hva.nl>
2025-10-08 20:45:50 +0200

    fix: I have put all the components in page.svelte

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,9 @@
 // place files you want to import through the `$lib` alias in this folder.
+export { default as Header } from './components/Header.svelte'
+export { default as Filter } from './components/Filter.svelte'
+export { default as Quote } from './components/Quote.svelte'
 export { default as Newsletter } from './components/Newsletter.svelte'
 export { default as Article } from './components/Article.svelte'
-export { default as Header } from './components/Header.svelte'
 export { default as Footer } from './components/Footer.svelte'
 export { default as FacebookLogo } from './components/FacebookLogo.svelte'
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,17 @@
 <script>
-    import { Newsletter } from '$lib';
-    export let artikelen = [];
-    import Filter from '$lib/components/Filter.svelte';
-
-    import { Article } from '$lib';
     import { Header } from '$lib';
+    export let artikelen = [];
+    import { Filter } from '$lib';
+    import { Quote } from '$lib';
+    import { Article } from '$lib';
+    import { Newsletter } from '$lib';
     import { Footer } from '$lib';
     import { FacebookLogo } from '$lib';
 </script>
 
+<Header/>
+<Filter/>
+<Quote/>
 <Newsletter/>
 <Article/>
-<Header/>
 <Footer/> 
