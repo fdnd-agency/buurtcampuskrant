47bc8b8cd747e8fbccacf981dc4ec6be20d27418
Akikosophia <asschermer@hotmail.com>
2025-11-11 18:59:58 +0100

    #67 fix: prevent duplicate article rendering and show correct category filter

--- src/routes/district/oost/+page.svelte
+++ src/routes/district/oost/+page.svelte
@@ -16,13 +16,22 @@
   {#if allStories.length > 0}
     <p>{allStories.length} artikelen gevonden</p>
   {/if}
+  <ul>
+    {#each allStories as story}
+      <li>
+        <strong>{story.title}</strong>
+        <p>{story.intro}</p>
+      </li>
+    {/each}
+  </ul>
   {:else}
-  <p>Geen artikelen gevonden</p>
+  <!-- <p>Geen artikelen gevonden</p> -->
 {/if}
 
 
+
 {#each categories as category}
-  {#if category.id == categoryId || !categoryId}
+  {#if category.id == +categoryId }
     {#if category.stories.length > 0}
       <!-- <h2>{category.title}</h2> -->
         <p>{category.stories.length} artikel{category.stories.length === 1 ? '' : 'en'} met het filter “{category.title || category.name}”</p>
