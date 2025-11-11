85ddbf27f8563ee5210b531943bb904dce2f174a
Akikosophia <asschermer@hotmail.com>
2025-11-11 15:33:05 +0100

    #67 feat: add district east

---
+++ src/routes/district/oost/+page.server.js
@@ -0,0 +1,35 @@
+export async function load() {
+
+    const district = "east";
+  
+    const [categoriesRes, storiesRes] = await Promise.all([
+      fetch("https://fdnd-agency.directus.app/items/buurtcampuskrant_categories"),
+      fetch(
+  `https://fdnd-agency.directus.app/items/buurtcampuskrant_stories?filter[district][_eq]=${district}&fields=*,categories.id`
+      ),
+    ]);
+  
+    const categoriesData = await categoriesRes.json();
+    const storiesData = await storiesRes.json();
+  
+    const categories = categoriesData.data || [];
+    const stories = storiesData.data || [];
+  
+      categories.forEach(cat => {
+        cat.stories = stories.filter(story =>
+          story.categories.some(c => c.id === cat.id)
+        );
+      });
+  
+      // console.log("categories:", categoriesData);
+      // console.log("stories:", storiesData);
+  
+      return {
+        categories,
+      
+      };
+  
+  
+     
+  }
+  
---
+++ src/routes/district/oost/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+  import Filter from "$lib/components/Filter.svelte";
+  export let data;
+  const { categories } = data;
+
+  import { page } from '$app/stores';
+  $: categoryId = $page.url.searchParams.get('category');
+
+</script>
+
+<Filter {categories}/>
+
+{#each categories as category}
+  {#if category.id == categoryId || !categoryId}
+    {#if category.stories.length > 0}
+      <!-- <h2>{category.title}</h2> -->
+        <p>{category.stories.length} artikel{category.stories.length === 1 ? '' : 'en'} met het filter “{category.title || category.name}”</p>
+      <ul>
+        {#each category.stories as story}
+          <li>
+            <strong>{story.title}</strong>
+            <p>{story.intro}</p>
+          </li>
+        {/each}
+      </ul>
+    {:else}
+      <p>Geen artikelen gevonden</p>
+    {/if}
+  {/if}
+{/each}
