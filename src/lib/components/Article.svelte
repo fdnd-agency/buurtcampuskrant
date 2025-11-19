7df7113dccd5a8e194cb974d0e833c652ea17525
Akikosophia <asschermer@hotmail.com>
2025-11-19 15:38:36 +0100

    #60 fix articles

--- src/lib/components/Article.svelte
+++ src/lib/components/Article.svelte
@@ -3,22 +3,26 @@
 </script>
 
 <article data-district="{article.district}">
-    <div class="container-content"> 
-<img 
-    src={`https://fdnd-agency.directus.app/assets/${article.cover}`}
-    alt="{article.alt}" 
-    width="280" 
-    height="150">
-        <div class="content">   
-            <h2>{ article.title }</h2>
-            <p>{ article.intro }</p>
+    <a href="/nieuws/{article.id}">
+        <div class="container">
+            <picture>
+                <source 
+                    srcset="https://fdnd-agency.directus.app/assets/{ article.cover }?format=webp&width=500"
+                    media="(min-width: 430px)"
+                    type="image/webp">
+                <img src="https://fdnd-agency.directus.app/assets/{ article.cover }?width=300" alt="">
+            </picture>
 
-            <div class="buttons">
-                <a href="/district/{ article.district}">{ article.district }</a>
-                <a href="/{ article.catogory}">{ article.catogory }</a>
+            <div class="content">   
+                <h2>{ article.title }</h2>
+
+                <div class="buttons">
+                    <button href="/district/{ article.district}">{ article.district }</button>
+                    <button href="/{ article.categories}">{ article.categories }</button>
+                </div>
             </div>
         </div>
-    </div>
+    </a>
 </article>
 
 <style>
@@ -36,52 +40,50 @@
         --primary-color-general: var(--secondary-color-south-east);
         --secondary-color-general: var(--primary-color-south-east);
     }
-    
-    article {
-        container-type: inline-size;
-        container-name: --article;
 
-        display: flex;
-        flex-direction: column;
+    article { 
         border-radius: 25px;
-        overflow: hidden;
         color: var(--secondary-color-general);       
         background-color: var(--primary-color-general);
         border: solid 3px var(--secondary-color-general);
+        overflow: hidden;
 
-        .container-content {
+        container-type: inline-size;
+        container-name: --article;
+
+        a {
+            text-decoration: none;
+            color: var(--secondary-color-general);
+            display: flex;
+            flex-direction: column;
 
             img {
                 width: 100%;
                 object-fit: cover;
-                min-height: 11.25rem;
+                object-position: center;
+                height: 100%;
+                max-height: 240px;
+                display: block;
             }
 
             .content {
                 display: flex;
                 flex-direction: column;
+                justify-content: space-between;
                 margin-inline: 1rem;
                 gap: var(--sm);
-                height: 100%;
 
                 h2 {
-                    font-size: 1.5rem;
+                    margin-top: 0.5rem;
+                    font-size: clamp(1.3rem, 5vw, 1.8rem);
                 }
-            
-                p {
-                    margin-block-start: 0;
-                    display: -webkit-box;
-                    -webkit-line-clamp: 5; 
-                    -webkit-box-orient: vertical;
-                    overflow: hidden;
-                }
-            
+
                 .buttons {
                     display: flex;
                     gap: var(--sm);
                     margin-bottom: 1em;
 
-                    a {
+                    button {
                         display: flex;
                         justify-content: center;
                         align-items: center;
@@ -92,11 +94,13 @@
                         text-decoration: none;
                     }
                 
-                    a:nth-of-type(1) {
+                    button:nth-of-type(1) {
                         background-color: var(--secondary-color-general);
                         color: var(--primary-color-general);
+                        border: none;
                     }
-                    a:nth-of-type(2) {
+                    
+                    button:nth-of-type(2) {
                         background-color: var(--primary-color-general);
                         color: var(--secondary-color-general);
                         border: solid 1px var(--secondary-color-general);
@@ -105,17 +109,25 @@
             }
         }
     }
-    
+
     @container --article (width > 430px) { 
-            
-            .container-content {
-                display: grid;
-                grid-template-columns: 40% 60%;
-
-                img {
-                    height: 100%;
-                }
-            }
+        .container {
+            display: grid;
+            grid-template-columns: 40% 60%;
         }
-</style>
+    }
 
+    @container --article (width < 230px) {
+        h2 {
+            font-size: var(--sm);
+        }
+
+        button {
+            font-size: 0.7rem;
+        }
+
+        .container img {
+            height: 150px;
+        }
+    }
+</style>
