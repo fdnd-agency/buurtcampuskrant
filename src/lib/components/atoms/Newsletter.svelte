bc0dcf9359af2e28971df81c92b62a65aed2ad41
RickFDND <rick.pijnacker@hva.nl>
2025-11-26 11:48:45 +0100

    feature: restyling because of adding container query #125

--- src/lib/components/atoms/Newsletter.svelte
+++ src/lib/components/atoms/Newsletter.svelte
@@ -4,26 +4,32 @@
 </script>
 
 <form method="post" use:enhance>
-    <label for="newsletter" class="sm">
-        Schrijf je in voor de nieuwsbrief!
-    </label>
-    <input type="email" name="email" placeholder=" e-mailadres" required id="newsletter">
-    <button type="submit">Aanmelden</button>
+    <div class="container">
+        <label for="newsletter" class="sm">
+            Schrijf je in voor de nieuwsbrief!
+        </label>
+        <input type="email" name="email" placeholder=" e-mailadres" required id="newsletter">
+        <button type="submit">Aanmelden</button>
+    </div>
 </form>
 
 <style>
-    label {
-        color: white;
-        grid-column: span 2;
-        font-size: var(--sm);
+    form {
+        container-type: inline-size;
+        container-name: --newsletter;
+        
+        width: 265px;
     }
 
-    form {
-        display: grid;
-        grid-template-columns: 2fr 1fr;
-        grid-template-rows: 1fr 1fr;
-        width: 14.375rem;
-        row-gap: var(--xs);
+    div {
+        display: flex;
+        flex-direction: column;
+
+        label {
+            color: white;
+            font-size: var(--sm);
+            padding-bottom: var(--xs);
+        }
 
         button {
             position: relative;
@@ -31,7 +37,6 @@
             justify-content: center;
             align-items: center;
             font-size: 0.7rem;
-            grid-row: 2;
             font-weight: 500;
             color: white;
             background-color: var(--accent-color);
@@ -44,32 +49,55 @@
                 background-color: var(--accent-color-darker);
             }
         }
+    }
 
-        button::after {
-            content: "";
-            position: absolute;
-            top: 0;
-            right: 0; 
-            width: 0;
-            height: 0;
-            border-top: 12px solid transparent;
-            border-bottom: 12px solid transparent;
-            border-right: 15px solid var(--primary-color-general);
-        }
+    input {
+        border: none;
+        border-style: solid;
+        border-color: #CACACA;
+        border-width: 1px 1px 1px 1px; 
+        grid-row: 2;
+        height: 1.5rem;
+    }
 
-        input {
-            border: none;
-            border-style: solid;
-            border-color: #CACACA;
-            border-width: 1px 0 1px 1px; 
-            grid-row: 2;
-            height: 1.5rem;
-        }
+    input::placeholder {
+        color: #CACACA;
+        font-style: italic;
+        font-size: 0.7rem;
+    }
 
-        input::placeholder {
-            color: #CACACA;
-            font-style: italic;
-            font-size: 0.7rem;
+    @container --newsletter (width > 264px) { 
+
+        div {
+            display: grid;
+            grid-template-columns: 2fr 1fr;
+            grid-template-rows: 1fr 1fr;
+            width: 14.375rem;  
+
+            label {
+                grid-column: span 2;
+            }
+            
+            button {
+                grid-row: 2;
+                grid-column: 2;
+            }
+
+            input {
+                border-width: 1px 0 1px 1px; 
+            }
+
+            button::after {
+                content: "";
+                position: absolute;
+                top: 0;
+                right: 0; 
+                width: 0;
+                height: 0;
+                border-top: 12px solid transparent;
+                border-bottom: 12px solid transparent;
+                border-right: 15px solid var(--primary-color-general);
+            }
         }
     }
 </style>
